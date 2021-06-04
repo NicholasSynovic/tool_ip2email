e3c8d7223843d2d569a28d0df014b109c680aa95
Nicholas M Synovic <nicholas.synovic@gmail.com>
2021-06-04 11:34:24 -0500

    Rename files

--- libs/test_cmdLineInterface.py
+++ libs/test_cmdLineInterface.py
@@ -1,9 +1,9 @@
 import unittest
 
-import ipAddress as ip
+import cmdLineInterface as cli
 
 
 class Test_IP_Address(unittest.TestCase):
     def test_CommandLineInterface(self):
         command = ["echo", "hello world"]
-        self.assertTrue(ip.commandLineInterface(command) == "hello world")
+        self.assertTrue(cli.commandLineInterface(command) == "hello world")

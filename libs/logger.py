299e5de52c6d0be45625415d423d2851bbd2feed
Nicholas M Synovic <nicholas.synovic@gmail.com>
2021-06-07 08:58:14 -0500

    Create test_logger.py

--- libs/logger.py
+++ libs/logger.py
@@ -1,14 +1,19 @@
 import time
+from datetime import datetime
 
 
-def createFile() -> None:
+def createFile() -> str:
     filename = "iptoemail_{}.txt".format(time.time())
+
     with open(file=filename, mode="w") as temp:
         temp.close()
 
+    return filename
 
-def logToFile(message: str, filename: str) -> bool:
-    message = "[{}]    {}".format(time.time(), message)
+
+def logToFile(message: str, filename: str) -> None:
+    currentTime = datetime.now().strftime("%H:%M:%S")
+    message = "[{}]    {}".format(currentTime, message)
 
     with open(file=filename, mode="a") as logfile:
         logfile.write(message)
